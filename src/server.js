cc76d6e610ded8e42a434491698e1666d68af651
Marcelo Luiz <mlluizpereira39@gmail.com>
2025-01-11 19:25:33 -0300

    update greeting message in root route

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!!!')
 })
 app.use('/auth', authRoutes)
 app.use('/todos', todoRoutes)
